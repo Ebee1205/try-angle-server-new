a85a11fc7c0eaae896bd589613ec2fdf77c33af2
Ebee1205 <spiverain02@gmail.com>
2026-04-25 18:04:04 +0900

    refact: common codes 맵핑하기 쉽게 enum 타입으로 변경

--- src/common/common_codes.py
+++ src/common/common_codes.py
@@ -1,10 +1,22 @@
-class ResponseStatus:
-    SUCCESS       = { "code": "S0000", "msg": "성공" }
-    ERROR         = { "code": "E0000", "msg": "에러 발생" }
-    BAD_REQUEST   = { "code": "E0001", "msg": "잘못된 요청입니다" }
-    UNAUTHORIZED  = { "code": "E0002", "msg": "인증이 필요합니다" }
-    FORBIDDEN     = { "code": "E0003", "msg": "접근이 거부되었습니다" }
-    NOT_FOUND     = { "code": "E0004", "msg": "리소스를 찾을 수 없습니다" }
-    CONFLICT      = { "code": "E0005", "msg": "이미 존재하는 리소스입니다" }
-    SERVER_ERROR  = { "code": "E0500", "msg": "서버 내부 오류가 발생했습니다" }
-    TIMEOUT       = { "code": "E0009", "msg": "응답 시간이 초과되었습니다" }
+from enum import Enum
+
+class ResponseStatus(Enum):
+    # (HTTP 상태코드, 내부 코드, 메시지)
+    SUCCESS      = (200, "S0000", "성공")
+    BAD_REQUEST  = (400, "E0001", "잘못된 요청입니다")
+    UNAUTHORIZED = (401, "E0002", "인증이 필요합니다")
+    FORBIDDEN    = (403, "E0003", "접근이 거부되었습니다")
+    NOT_FOUND    = (404, "E0004", "리소스를 찾을 수 없습니다")
+    CONFLICT     = (409, "E0005", "이미 존재하는 리소스입니다")
+    TIMEOUT      = (408, "E0009", "응답 시간이 초과되었습니다")
+    SERVER_ERROR = (500, "E0500", "서버 내부 오류가 발생했습니다")
+
+    def __init__(self, http_code, code, msg):
+        self.http_code = http_code
+        self.info = {"code": code, "msg": msg}
+
+    @classmethod
+    def from_http_code(cls, http_code):
+        """HTTP 코드로 해당 Enum 상수를 찾는 팩토리 메서드"""
+        # 다음은 파이썬의 Generator expression을 사용한 효율적인 검색입니다.
+        return next((status for status in cls if status.http_code == http_code), cls.SERVER_ERROR)
